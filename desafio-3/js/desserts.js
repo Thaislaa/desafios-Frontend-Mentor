9e502acbf894d297f7e7481bc8f260d3153317a2
Thaislaa <thaisladaveiga@gmail.com>
2026-02-11 14:45:59 -0300

    Removendo o campo quantity da lista de desserts

--- desafio-3/js/desserts.js
+++ desafio-3/js/desserts.js
@@ -3,63 +3,54 @@ export const desserts = [
         category: "Waffle",
         title: "Waflle with Berries",
         price: 6.50,
-        image: "./images/image-waffle-desktop.jpg",
-        quantity: 0
+        image: "./images/image-waffle-desktop.jpg"
     },
     {
         category: "Crème Brûlée",
         title: "Vanilla Bean Crème Brûlée",
         price: 7.00,
-        image: "./images/image-creme-brulee-desktop.jpg",
-        quantity: 0
+        image: "./images/image-creme-brulee-desktop.jpg"
     },
     {
         category: "Macaron",
         title: "Macaron Mix of Five",
         price: 8.00,
-        image: "./images/image-macaron-desktop.jpg",
-        quantity: 0
+        image: "./images/image-macaron-desktop.jpg"
     },
     {
         category: "Tiramisu",
         title: "Classic Tiramisu",
         price: 5.50,
-        image: "./images/image-tiramisu-desktop.jpg",
-        quantity: 0
+        image: "./images/image-tiramisu-desktop.jpg"
     },
     {
         category: "Baklava",
         title: "Pistachio Baklava",
         price: 7.00,
-        image: "./images/image-baklava-desktop.jpg",
-        quantity: 0
+        image: "./images/image-baklava-desktop.jpg"
     },
     {
         category: "Pie",
         title: "Lemon Meringue Pie",
         price: 5.00,
-        image: "./images/image-meringue-desktop.jpg",
-        quantity: 0
+        image: "./images/image-meringue-desktop.jpg"
     },
     {
         category: "Cake",
         title: "Red Velvet Cake",
         price: 4.50,
-        image: "./images/image-cake-desktop.jpg",
-        quantity: 0
+        image: "./images/image-cake-desktop.jpg"
     },
     {
         category: "Brownie",
         title: "Salted Caramel Brownie",
         price: 5.50,
-        image: "./images/image-brownie-desktop.jpg",
-        quantity: 0
+        image: "./images/image-brownie-desktop.jpg"
     },
     {
         category: "Panna Cotta",
         title: "Vanilla Panna Cotta",
         price: 6.50,
-        image: "./images/image-panna-cotta-desktop.jpg",
-        quantity: 0
+        image: "./images/image-panna-cotta-desktop.jpg"
     }
 ];
