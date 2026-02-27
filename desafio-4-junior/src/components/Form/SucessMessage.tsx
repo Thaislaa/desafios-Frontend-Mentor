2c1378bbeabe1cfb8b42d100695ba0e356e48dd5
Thaislaa <thaisladaveiga@gmail.com>
2026-02-27 08:37:33 -0300

    Organização do formulário em componentes

---
+++ desafio-4-junior/src/components/Form/SucessMessage.tsx
@@ -0,0 +1,21 @@
+import check from "../../../assets/images/icon-success-check.svg";
+
+interface SucessMessageProps {
+    visible: boolean
+}
+
+export function SucessMessage({ visible }: SucessMessageProps) {
+    return (
+        <>
+            {
+                visible && (
+                    <div className="sucess-div">
+                        <p className="message-check"><img src={check} alt="Check" className="padding-right" />Message Sent!</p>
+                        <p className="thanks-message">Thanks for completing the form. We'll be in touch soon!
+                        </p>
+                    </div>
+                )
+            }
+        </>
+    )
+}
